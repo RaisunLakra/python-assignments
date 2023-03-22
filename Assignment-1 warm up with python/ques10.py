2856b1bcb8f04fbf402592289546dd2ba2ee4258
Raisun <raisunlakra18@gmail.com>
2023-03-22 11:15:59 +0530

    Add question 9 to 10

---
+++ Assignment-1 warm up with python/ques10.py
@@ -0,0 +1,7 @@
+# Write a python script to print values of three variables, each in a new line. Variable contains integer values
+a=1
+b=2
+c=3
+print(a)
+print(b)
+print(c)
---
+++ Assignment-1 warm up with python/ques9.py
@@ -0,0 +1,9 @@
+# Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name="Raisun Lakra"
+age=24
+qualification="B.sc(Mathematics)"
+yearsOfExperience=3
+print(name)
+print(age)
+print(qualification)
+print(yearsOfExperience)
