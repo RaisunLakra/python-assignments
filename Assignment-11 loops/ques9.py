b9421c53619556c09cb207423335cb5a1603e306
Raisun <raisunlakra18@gmail.com>
2023-03-29 03:44:01 +0530

    updated ques9

--- Assignment-11 loops/ques9.py
+++ Assignment-11 loops/ques9.py
@@ -5,25 +5,21 @@ use bin() method)
 
 num = float(input("Enter a decimal number: "))
 integer_part, fractional_part = divmod(num, 1)
-print(integer_part)
-print(fractional_part)
+
 binary_integer = ''
 binary_fractional = ''
 
-integer_part=int(integer_part)
-
 while integer_part > 0:
     quotient, remainder = divmod(integer_part, 2)
-    binary_integer = str(remainder) + binary_integer
+    binary_integer = str(int(remainder)) + binary_integer
     integer_part = quotient
-print(binary_integer)
 
 if fractional_part != 0:
     binary_fractional = '.'
     for i in range(10):  # set the precision to 10 decimal places
-        # fractional_part *= 2
         integer_part, fractional_part = divmod(fractional_part*2, 1)
         binary_fractional += str(int(integer_part))
+        fractional_part=remainder
 
 binary = binary_integer + binary_fractional
 
